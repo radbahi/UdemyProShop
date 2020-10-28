ff22a2841a7cc7e3168dec29aca071748c9000b0
Radouane Bahi <radbahi@gmail.com>
2020-10-27 22:09:36 -0400

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to ProShop</h1>
+    </>
+  )
+}
+
+export default App
+
+// changed App to arrow function per Brad's preference
+// deleted entire div that was here from create react app boiler plate
+// deleted initial .git folder using command "rm -rf .git". wants git repository in root, not in frontend
+// move .gitignore into root
+// added "node_modules/" into gitignore because all node modules are going to be in root, which is all of our server dependecies
+// added .env to gitignore, too
+//https://www.udemy.com/course/mern-ecommerce/learn/lecture/22484732#questions
