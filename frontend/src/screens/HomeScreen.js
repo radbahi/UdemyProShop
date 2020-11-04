16ba3aaa44ad3a2373e910cdff6d9318eb34332d
Radouane Bahi <radbahi@gmail.com>
2020-11-04 18:31:58 -0500

    setting up fetch hooks. url error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react' // useState hook is to use state in functional components since only class components have constructor
+// whatever is in useEffect runs as soon as the component loads. good for api fetches
 import products from '../products'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios' // powerful library for fetches
 
 const HomeScreen = () => {
+  const [product, setProducts] = useState([])
+
+  useEffect(() => {
+    // better to use async await than .then. read this for why https://stackoverflow.com/a/54497100/13371788
+    // can't directly call async on useEffect so we have to make a new variable
+    const fetchProducts = async () => {
+      const { data } = await axios.get('api/products') // we added a proxy to frontend's package.json to prevent cors errors
+      // data is destructured above so we can call it directly for below instead of doing res.data all the time. this is optional.
+      setProducts(data)
+      // now we set that data to the state of this component
+    }
+    fetchProducts() // now just call it
+  }, []) // this is an array of dependencies. anything u want to fire useEffect off when it changes gets put in there. don't need anything here, though.
+
   return (
     <>
       <h1>Latest products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react' // useState hook is to use state in functional components since only class components have constructor
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
   // A match object contains information about how a <Route path> matched the URL. https://reactrouter.com/web/api/match#:~:text=A%20match%20object%20contains%20information,was%20matched%20(no%20trailing%20characters)
-  const product = products.find((p) => p._id === match.params.id) // id is a param from the url accessed by the match object
+  const [product, setProduct] = useState({}) // product is an object which is why we use braces here
+
+  useEffect(() => {
+    // better to use async await than .then. read this for why https://stackoverflow.com/a/54497100/13371788
+    // can't directly call async on useEffect so we have to make a new variable
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`api/products/${match.params.id}`) // we added a proxy to frontend's package.json to prevent cors errors
+      // id is a param from the url accessed by the match object
+      // data is destructured above so we can call it directly for below instead of doing res.data all the time. this is optional.
+      setProduct(data)
+      // now we set that data to the state of this component
+    }
+    fetchProduct() // now just call it
+  }, [match.params.id]) // this is an array of dependencies. anything u want to fire useEffect off when it changes gets put in there.
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
