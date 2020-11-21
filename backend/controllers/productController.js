795fe37b76925a4a92faffa46400d2455c94e63a
Radouane Bahi <radbahi@gmail.com>
2020-11-21 15:57:51 -0500

    productbyid infinite fetch fixed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,17 +14,15 @@ const getProducts = asyncHandler(async (req, res) => {
 //@route GET /api/products/:id
 //@access public
 const getProductById = asyncHandler(async (req, res) => {
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    // req.params.id gets its value from the url
+  const product = await Product.findById(req.params.id)
+  // req.params.id gets its value from the url
 
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
 })
 
 export { getProducts, getProductById }
