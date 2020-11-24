69c0f59c40c42468520eb576854bd8ab00140a34
Radouane Bahi <radbahi@gmail.com>
2020-11-24 13:09:56 -0500

    auth middleware added

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token // we don't include a value here to make it default to false for the if statement on line 19
+  //token gets defined if there's an auth header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer') // WHY DO WE WANT BEARER IN THE TOKEN??
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1] // token comes through as "Bearer token" so split the space into array and get token element
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = await User.findById(decoded.id).select('-password') //we don't want the password to be sent to define req.user
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized. Token failed.')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized. No token.')
+  }
+})
+
+export { protect }
+//this gets imported to userRoutes
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/login', authUser) // /login is hooked to /api/users
+router.route('/profile').get(protect, getUserProfile) //.route method used for doing more than one verb?
 
 export default router
