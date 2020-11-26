82cc866a31bda136f7fc8a05f8c41bb8e81eaba1
Radouane Bahi <radbahi@gmail.com>
2020-11-26 13:32:18 -0500

    login screen ACTUALLY done

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -5,17 +5,38 @@ import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions.js'
 
-const LoginScreen = ({ location }) => {
+const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
+
   const [password, setPassword] = useState('')
+
   const redirect = location.search ? location.search.split('=')[1] : '/' //location.search has the url query string. its brought in as a prop
+
   const submitHandler = (e) => {
     e.preventDefault()
-    //DISPATCH LOGIN
+    dispatch(login(email, password))
   }
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin) //grab userLogin which gets its data from userLoginReducer...
+
+  const { loading, error, userInfo } = userLogin //...then deconstruct and get these from the state
+
+  // check if user has logged in then redirects
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && (
+        <h2 style={{ color: 'red', backgroundColor: 'yellow' }}>{error}</h2>
+      )}
+      {loading && <h1>Loading...</h1>}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
@@ -31,7 +52,7 @@ const LoginScreen = ({ location }) => {
           <Form.Control
             type='password'
             placeholder='Enter password'
-            value={email}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
