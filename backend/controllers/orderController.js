d14ca844afb06b9634087a494f6d688ae06abf5e
Radouane Bahi <radbahi@gmail.com>
2020-12-02 13:07:05 -0500

    order backend implemented

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+// we previously had everything in the routes files but routes should only handle methods. controllers should handle functionality. \
+import asyncHandler from 'express-async-handler' // library for error handling for express async methods
+import Order from '../models/orderModel.js'
+
+//@desc create new order
+//@route POST /api/orders
+//@access private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    shippingAddress,
+    orderItems,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No Order items')
+    return
+  } else {
+    const order = new Order({
+      user: req.user._id,
+      shippingAddress,
+      orderItems,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
+
+    const createdOrder = await order.save()
+    res.status(201).json(createdOrder)
+  }
+})
+
+export default addOrderItems
